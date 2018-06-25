025049784526955099e7bb14d1e9640b8bc9fed5
David <ikari@davidsuarez.com.ar>
2018-06-25 17:12:36 -0300

    Don´t hidden menu bar if windows MenuBarAutoHide is false

--- src/WindowMenu.js
+++ src/WindowMenu.js
@@ -1,86 +1,87 @@
-const Menu = require('./Menu');
+const Menu = require("./Menu");
 
 /**
  * Menu that only render for current window.
  * @type {ReactClass}
  */
 class WindowMenu extends Menu {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.onFocus = this.onFocus.bind(this);
-        this.onBlur = this.onBlur.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+  }
+
+  componentDidMount() {
+    this.onMenuUpdated();
+  }
+
+  componentDidUpdate() {
+    this.onMenuUpdated();
+  }
+
+  componentWillUnmount() {
+    const { electron } = this.context;
+    const win = electron.remote.getCurrentWindow();
+
+    if (win) {
+      this.clearListeners(win);
+      if (win.isFocused()) this.onBlur();
+    }
+  }
+
+  /**
+   * Clear listener for a window.
+   * @param {Window} win
+   */
+  clearListeners(win) {
+    win.removeListener("focus", this.onFocus);
+    win.removeListener("blur", this.onBlur);
+  }
+
+  /**
+   * Update the menu for the window.
+   */
+  onMenuUpdated() {
+    const { electron } = this.context;
+    const win = electron.remote.getCurrentWindow();
+
+    if (!win) {
+      return;
     }
 
-    componentDidMount() {
-        this.onMenuUpdated();
+    this.clearListeners(win);
+
+    win.on("focus", this.onFocus);
+    win.on("blur", this.onBlur);
+
+    if (win.isFocused() || document.hasFocus()) {
+      this.onFocus();
     }
+  }
 
-    componentDidUpdate() {
-        this.onMenuUpdated();
+  /**
+   * When window is focused.
+   */
+  onFocus() {
+    const { electron } = this.context;
+    const menu = this.getMenu();
+
+    electron.remote.Menu.setApplicationMenu(menu);
+  }
+
+  /**
+   * When window is focused.
+   */
+  onBlur() {
+    const { electron } = this.context;
+    const currentMenu = electron.remote.Menu.getApplicationMenu();
+    const win = electron.remote.getCurrentWindow();
+
+    if (currentMenu == this.menu && win.isMenuBarAutoHide()) {
+      electron.remote.Menu.setApplicationMenu(null);
     }
-
-    componentWillUnmount() {
-        const { electron } = this.context;
-        const win = electron.remote.getCurrentWindow();
-
-        if (win) {
-            this.clearListeners(win);
-            if (win.isFocused()) this.onBlur();
-        }
-    }
-
-    /**
-     * Clear listener for a window.
-     * @param {Window} win
-     */
-    clearListeners(win) {
-        win.removeListener('focus', this.onFocus);
-        win.removeListener('blur', this.onBlur);
-    }
-
-    /**
-     * Update the menu for the window.
-     */
-    onMenuUpdated() {
-        const { electron } = this.context;
-        const win = electron.remote.getCurrentWindow();
-
-        if (!win) {
-            return;
-        }
-
-        this.clearListeners(win);
-
-        win.on('focus', this.onFocus);
-        win.on('blur', this.onBlur);
-
-        if (win.isFocused() || document.hasFocus()) {
-            this.onFocus();
-        }
-    }
-
-    /**
-     * When window is focused.
-     */
-    onFocus() {
-        const { electron } = this.context;
-        const menu = this.getMenu();
-
-        electron.remote.Menu.setApplicationMenu(menu);
-    }
-
-    /**
-     * When window is focused.
-     */
-    onBlur() {
-        const { electron } = this.context;
-        const currentMenu = electron.remote.Menu.getApplicationMenu();
-
-        if (currentMenu == this.menu) {
-            electron.remote.Menu.setApplicationMenu(null);
-        }
-    }
+  }
 }
 
 WindowMenu.contextTypes = Menu.contextTypes;
